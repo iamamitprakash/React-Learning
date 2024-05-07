48284663985a76ab85e5418e5ca1139abe0f1b9e
Amit Prakash <prakashamit37@gmail.com>
2024-05-07 13:34:38 +0530

    Create a project using npx command

---
+++ Test-App/test-app/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React!!!</h1>
+      <h2>Learning React is a beneficial to make an App!!!</h2>
+      <h3>If You Want To Make A Web App, You Just Learn It And Getting Master In It!!!</h3>
+    </div>
+  );
+}
+
+export default App;
